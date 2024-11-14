5d909dab415d05e9861649566821315466a203a0
Prashuk Jain <jainprashuk5012258@gmail.com>
2024-11-14 17:12:36 +0530

    Model_for_User

--- MemoRies/accounts/urls.py
+++ MemoRies/accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from accounts.views import ForgottenPasswordView, ChangePasswordView, CustomTokenObtainPairView, RegisterUserView, \
+    ActivateAccountView, CustomTokenRefreshView
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterUserView.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/',
+            ActivateAccountView.as_view(), name='activate_account'),
+
+    path('reset/password/', ForgottenPasswordView.as_view(), name='forgotten_password'),
+    path('set/password/<uidb64>/<token>/', ChangePasswordView.as_view(), name='password_reset_confirm'),
 ]
